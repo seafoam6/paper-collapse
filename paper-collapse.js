33a30469c9a512313ef59f5721e9bb11bc5ac2a4
Steve Barman <steve@stevebarman.com>
2015-02-25 10:53:57 -0500

    more accordion changes

--- paper-collapse.js
+++ paper-collapse.js
@@ -20,8 +20,10 @@
           $(this).closest('.collapse-card').removeClass('active');
           $(this).closest('.collapse-card').find('.collapse-card__body').slideUp(settings.animationDuration, settings.onHideComplete);
         } else {
-          $('.collapse-card').removeClass('active');
-          $('.collapse-card').find('.collapse-card__body').slideUp(settings.animationDuration, settings.onHideComplete);
+          if (settings.closeOthers == true){
+            $('.collapse-card').removeClass('active');
+            $('.collapse-card').find('.collapse-card__body').slideUp(settings.animationDuration, settings.onHideComplete);
+          }
           settings.onShow.call(this);
           $(this).closest('.collapse-card').addClass('active');
           $(this).closest('.collapse-card').find('.collapse-card__body').slideDown(settings.animationDuration, settings.onShowComplete);
@@ -32,6 +34,7 @@
     $.fn.paperCollapse.defaults = {
       animationDuration: 400,
       easing: 'swing',
+      closeOthers: false,
       closeHandler: '.collapse-card__close_handler',
       onShow: function() {},
       onHide: function() {},
